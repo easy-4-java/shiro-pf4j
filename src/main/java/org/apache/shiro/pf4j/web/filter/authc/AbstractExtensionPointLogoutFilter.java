432053cec7798349266bc88994432eb23d48dde6
wandl-6A72h <hiwepy@gmail.com>
2026-08-06 15:00:11 +0800

    chore: 注释作者信息统一为 @Loong Wan

--- src/main/java/org/apache/shiro/pf4j/web/filter/authc/AbstractExtensionPointLogoutFilter.java
+++ src/main/java/org/apache/shiro/pf4j/web/filter/authc/AbstractExtensionPointLogoutFilter.java
@@ -11,7 +11,7 @@ import org.pf4j.PluginManager;
 
 /**
  * 扩展Shiro登出逻辑，增加监听回调接口
- * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public abstract class AbstractExtensionPointLogoutFilter extends AbstractLogoutFilter {
 
